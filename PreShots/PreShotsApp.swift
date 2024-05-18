24c74506d854b3cd98d9a87a5bd31849e2f84856
Terry Kuo <terrykuo501@gmail.com>
2024-05-19 00:14:29 +0800

    Initial Commit

---
+++ PreShots/PreShotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreShotsApp.swift
+//  PreShots
+//
+//  Created by Terry Kuo on 2024/5/19.
+//
+
+import SwiftUI
+
+@main
+struct PreShotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
